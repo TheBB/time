77f0a1c4b0a35dbf8bd3fdb77dcf3de37bac0f31
Eivind Fonn <evfonn@gmail.com>
2013-07-26 09:52:42 +0200

    More stuff.

--- src/timekeeper.py
+++ src/timekeeper.py
@@ -67,12 +67,14 @@ class TimekeeperWindow(pyglet.window.Window):
                                            x=5, y=self.height-15)
         self.loc_label = pyglet.text.Label('', font_name='Monospace', font_size=10,
                                            x=5, y=self.height-30)
+        self.jd_label = pyglet.text.Label('', font_name='Monospace', font_size=10,
+                                           x=self.width/2+5, y=self.height-15)
 
         self.update()
         self.construct_system()
         self.recalc_system()
 
-        pyglet.clock.schedule_interval(self.update, 0.1)
+        pyglet.clock.schedule_interval(self.update, 0.5)
 
     def construct_system(self):
         self.sun_vl = pyglet.graphics.vertex_list(30,
@@ -81,19 +83,19 @@ class TimekeeperWindow(pyglet.window.Window):
         )
 
         self.orbit_vl = pyglet.graphics.vertex_list(60, 'v2f', ('c3B', [160, 160, 160] * 60))
-        self.helion_vl = pyglet.graphics.vertex_list(2, 'v2f', ('c3B', [80, 80, 80] * 2))
+        self.helion_vl = pyglet.graphics.vertex_list(2, 'v2f', ('c3B', [70, 70, 70] * 2))
         self.perihelion_label = pyglet.text.Label('Perihelion', font_name=FONT, font_size=10,
             anchor_x='center', anchor_y='center')
         self.aphelion_label = pyglet.text.Label('Aphelion', font_name=FONT, font_size=10,
             anchor_x='center', anchor_y='center')
 
-        self.equinox_vl = pyglet.graphics.vertex_list(2, 'v2f', ('c3B', [80, 80, 80] * 2))
+        self.equinox_vl = pyglet.graphics.vertex_list(2, 'v2f', ('c3B', [70, 70, 70] * 2))
         self.autumnal_label = pyglet.text.Label('Autumnal equinox', font_name=FONT, font_size=10,
             anchor_x='center', anchor_y='center')
         self.vernal_label = pyglet.text.Label('Vernal equinox', font_name=FONT, font_size=10,
             anchor_x='center', anchor_y='center')
 
-        self.solstice_vl = pyglet.graphics.vertex_list(2, 'v2f', ('c3B', [80, 80, 80] * 2))
+        self.solstice_vl = pyglet.graphics.vertex_list(2, 'v2f', ('c3B', [70, 70, 70] * 2))
         self.south_label = pyglet.text.Label('Southern solstice', font_name=FONT, font_size=10,
             anchor_x='center', anchor_y='center')
         self.north_label = pyglet.text.Label('Northern solstice', font_name=FONT, font_size=10,
@@ -129,19 +131,26 @@ class TimekeeperWindow(pyglet.window.Window):
 
     def update(self, dt=0.0):
         utc = datetime.utcnow() + self.adjustment
+
         offset = get_offset()
         loc = utc + timedelta(seconds=offset)
 
+        a = (14 - utc.month) / 12
+        y = utc.year + 4800 - a
+        m = utc.month + 12*a - 3
+        jdn = utc.day + (153*m+2)/5 + 365*y + y/4 - y/100 + y/400 - 32045
+        self.jd = jdn + (utc.hour-12)/24. + utc.minute/1440. + utc.second/86400.
+
         self.utc_label.text = utc.strftime('UTC:   ' + TIME_FORMAT)
         self.loc_label.text = loc.strftime('Local: ' + TIME_FORMAT) + ' (UTC %+05d)' % (offset/36)
+        self.jd_label.text = 'Julian date: %.5f' % self.jd
 
     def on_draw(self):
         self.clear()
 
-        #glLoadIdentity()
-
         self.utc_label.draw()
         self.loc_label.draw()
+        self.jd_label.draw()
 
         pyglet.graphics.draw(2, GL_LINE_STRIP, ('v2i', (0, self.height-35, self.width, self.height-35)))
 
